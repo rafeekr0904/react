8686ce4588d1974d46a39fcf30eba13dee874ace
rafeek <rafeekr09@gmail.com>
2020-02-27 17:10:45 +0400

    Updates

---
+++ precache-manifest.4b6b1b45e1436a1e9990cf2ca7fa146a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76178e6c8a73bff26836469b51d11a5",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "ea0b41ca544b3c9426ac",
+    "url": "/react/static/css/main.7ce419cd.chunk.css"
+  },
+  {
+    "revision": "8daf9862bf683eaa785d",
+    "url": "/react/static/js/2.53f63a6a.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/react/static/js/2.53f63a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0b41ca544b3c9426ac",
+    "url": "/react/static/js/main.ab475424.chunk.js"
+  },
+  {
+    "revision": "d6814f6ac82768cf6611",
+    "url": "/react/static/js/runtime-main.d7bd19ea.js"
+  }
+]);
